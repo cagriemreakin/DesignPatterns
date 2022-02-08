84d0efd417ca2dc410664929539c02b4a0853cbd
Çağrı Emre AKIN <cagriemreakin@gmail.com>
2022-02-08 12:04:46 +0300

    Add project files.

---
+++ AppIdentity/Models/AppIdentityDbcontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseApp.Models
+{
+    public class AppIdentityDbcontext:IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbcontext(DbContextOptions<AppIdentityDbcontext> options):base(options)
+        {
+
+        }
+    }
+}
